24a1947fb49d8d77b0b74604f6fef6e532442ef8
andrioddeveloper <android.developer.70@gmail.com>
2024-03-10 14:57:59 +0330

    Initial Commit

---
+++ nera/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  nera
+//
+//  Created by Andriod Developer on 12/20/1402 AP.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ nera/neraApp.swift
@@ -0,0 +1,17 @@
+//
+//  neraApp.swift
+//  nera
+//
+//  Created by Andriod Developer on 12/20/1402 AP.
+//
+
+import SwiftUI
+
+@main
+struct neraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
